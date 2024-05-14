cbb1b0470d946ee23d2722819891ec19322302c8
deadaj1707 <95641989+deadaj1707@users.noreply.github.com>
2024-05-15 00:56:58 +0530

    Update db.py

--- config/db.py
+++ config/db.py
@@ -1,3 +1,3 @@
 from pymongo import MongoClient
-MONGO_URI: str="mongodb+srv://jhaaditya1707:abcd1234@cluster0.brdbnmc.mongodb.net/notes"
+MONGO_URI: str="mongodb+srv://username:password@cluster0.brdbnmc.mongodb.net/notes"
 conn= MongoClient(MONGO_URI)
